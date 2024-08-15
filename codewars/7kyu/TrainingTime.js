19241246e22d7159701f51274d114a48e50ba5f3
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2024-08-15 23:57:40 +0400

    codewars: solved problem, 7kyu
    https://www.codewars.com/kata/572ab0cfa3af384df7000ff8

---
+++ codewars/7kyu/TrainingTime.js
@@ -0,0 +1,12 @@
+/* https://www.codewars.com/kata/572ab0cfa3af384df7000ff8 */
+function shuffleIt(array, ...otherArguments) {
+	for (let i = 0; i < otherArguments.length; i++) {
+		let a = otherArguments[i];
+		let temp = array[a[0]];
+
+		array[a[0]] = array[a[1]];
+		array[a[1]] = temp;
+	}
+
+	return array;
+}
