b7e0ce351abdb14e0de235a2f2d7549e63ef2297
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2024-08-19 13:30:03 +0400

    javascript(info): added 4.2

---
+++ javascript.info/chapter IV/copying.js
@@ -0,0 +1,78 @@
+/** 4.2 Objects merging and copying
+ * Copying
+ * * object copied -> reference copied -> but object itself isn't duplicated.
+ * Cloning
+ * * Shallow copy
+ * * * for...in
+ * * * * for (const key in main) { clone[key] = main[key] } // clone === main is true
+ * * * * will have same reference on object
+ * * * Object.assign(targetObj, otherObjs)
+ * * * * if copied name exists -> overwrite it
+ * * * * will have same reference on object
+ * * Nested cloning
+ * * * structuredClone(obj)
+ * * * * can't copy methods
+ * * * JSON.parse(JSON.stringify(obj)) -> good
+ * * * lodash _.cloneDeep(obj) -> good
+ */
+// Same objects
+const obj1 = {};
+const obj2 = obj1;
+obj1 === obj2; // true
+// Different objects
+const obj3 = {};
+const obj4 = {};
+obj3 === obj4; // false
+
+/* Clonning */
+const mainObject = {
+	a: 1,
+	b: 2,
+	c: {
+		d: 3,
+		e: 4,
+		f: () => {}
+	},
+	arr: [1, 2, 3]
+};
+/* Clonning: shallow copy */
+// for...in
+console.log("===for...in===");
+const cloneForinObject = {};
+
+for (const key in mainObject) {
+	cloneForinObject[key] = mainObject[key];
+}
+console.log(cloneForinObject === mainObject); // false, different!
+console.log(cloneForinObject.c === mainObject.c); // true, not copied nested object
+console.log(cloneForinObject.arr === mainObject.arr); // true, not copied nested array
+// Object.assign
+console.log("===Object.assign===");
+const cloneAssignObject = {};
+
+Object.assign(cloneAssignObject, mainObject);
+console.log(cloneAssignObject === mainObject); // false, different!
+console.log(cloneAssignObject.c === mainObject.c); // true, not copied nested object
+console.log(cloneAssignObject.arr === mainObject.arr); // true, not copied nested array
+/* Clonning: nested copy */
+console.log("===structuredClone===");
+delete mainObject.c.f; // exception with methods
+const cloneStructuredcloneObject = structuredClone(mainObject);
+
+console.log(cloneStructuredcloneObject === mainObject); // false, different!
+console.log(cloneStructuredcloneObject.c === mainObject.c); // false, different!
+console.log(cloneStructuredcloneObject.arr === mainObject.arr); // false, different!
+/* Lodash */
+mainObject.c.f = () => {};
+console.log("===lodash===");
+import _ from "lodash";
+const cloneLodashObject = _.cloneDeep(mainObject);
+console.log(cloneLodashObject === mainObject); // false, different!
+console.log(cloneLodashObject.c === mainObject.c); // false, different!
+console.log(cloneLodashObject.arr === mainObject.arr); // false, different!
+/* JSON.parse(JSON.stringify()) */
+console.log("===JSON===");
+const cloneJSONObject = JSON.parse(JSON.stringify(mainObject));
+console.log(cloneJSONObject === mainObject); // false, different!
+console.log(cloneJSONObject.c === mainObject.c); // false, different!
+console.log(cloneJSONObject.arr === mainObject.arr); // false, different!
