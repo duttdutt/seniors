9d7bf5290345438e8e574299e9908794a0237ab2
ynk <ynk@ynks-MacBook-Air.local>
2024-09-14 00:59:02 +0300

    codewars: solved 7kyu

---
+++ codewars/7kyu/HighestAndLowest.js
@@ -0,0 +1,6 @@
+// www.codewars.com/kata/554b4ac871d6813a03000035
+https: function highAndLow(numbers) {
+    const arr = numbers.split(" ").map(v => +v);
+
+    return `${Math.max(...arr)} ${Math.min(...arr)}`;
+}
