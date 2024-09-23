f0a5d55556dd64560c87de9d44d4f2e8b96618bd
yNk <ynk1654@gmail.com>
2024-09-23 21:11:25 +0300

    codewars: solved 5kyu problem

---
+++ codewars/5kyu/Flatten.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/513fa1d75e4297ba38000003
+function flatten(...stack) {
+  const result = [];
+  
+  while (stack.length) {
+    const el = stack.shift();
+    
+    if (Array.isArray(el)) {
+      stack.unshift(...el)
+      continue;
+    }
+
+    result.push(el)
+  }
+  
+  return result
+}
