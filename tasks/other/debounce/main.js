0bae70c89073084282b656713ba26b55c9d794d5
duttdutt <ynksdk1654@gmail.com>
2024-10-04 10:16:16 +0300

    tasks: added debounce/throttle differences

---
+++ tasks/other/debounce/main.js
@@ -0,0 +1,64 @@
+/* --- Getting Elements --- */
+// Default Elements
+const inputDefault = document.getElementById("input-default");
+const buttonDefault = document.getElementById("button-default");
+const outputDefault = document.getElementById("output-default");
+// Debounce Elements
+const inputDebounce = document.getElementById("input-debounce");
+const buttonDebounce = document.getElementById("button-debounce");
+const outputDebounce = document.getElementById("output-debounce");
+// Throttle Elements
+const inputThrottle = document.getElementById("input-throttle");
+const buttonThrottle = document.getElementById("button-throttle");
+const outputThrottle = document.getElementById("output-throttle");
+
+/* --- Default Event --- */
+buttonDefault.addEventListener("click", () => {
+    outputDefault.textContent = "";
+    outputDefault.textContent = inputDefault.value;
+});
+
+/* --- Debounce Function --- */
+function debounce(func, duration) {
+    let timeout;
+
+    return function (...args) {
+        const effect = () => {
+            timeout = null;
+            return func.apply(this, args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(effect, duration);
+    };
+}
+
+const debouncedEvent = debounce(() => {
+    outputDebounce.textContent = "";
+    outputDebounce.textContent = inputDebounce.value;
+}, 1000);
+
+buttonDebounce.addEventListener("click", debouncedEvent);
+
+/* --- Throttle Function --- */
+function throttle(func, duration) {
+    let shouldWait = false;
+
+    return function (...args) {
+        if (!shouldWait) {
+            func.apply(this, args);
+            shouldWait = true;
+
+            setTimeout(function () {
+                shouldWait = false;
+            }, duration);
+        }
+    };
+};
+
+const throttledEvent = throttle(() => {
+    outputThrottle.textContent = "";
+    outputThrottle.textContent = inputThrottle.value;
+}, 2000)
+
+buttonThrottle.addEventListener("click", throttledEvent)
