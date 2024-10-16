6e6f5222de1d03555c50c6e80bcd0f1fe7934b8d
duttdutt <ynksdk1654@gmail.com>
2024-10-16 14:00:38 +0300

    codewars: solved 6kyu problem

--- codewars/6kyu/ArrayDiff.js
+++ codewars/6kyu/ArrayDiff.js
@@ -1,5 +1,9 @@
 //www.codewars.com/kata/523f5d21c841566fde000009/
 function arrayDiff(a, b) {
-    const set = new Set(b);
-    return a.filter(item => !set.has(item));
+  const set = new Set(b);
+  return a.filter((item) => !set.has(item));
+}
+
+class User {
+  constructor() {}
 }
